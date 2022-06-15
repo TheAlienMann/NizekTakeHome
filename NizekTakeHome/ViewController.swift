a448253e23a10a7e3c63818312dbb3324af30755
TheAlienMann <rahmanipour.mohammad@gmail.com>
2022-06-15 17:47:07 +0430

    Initial Commit

---
+++ NizekTakeHome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NizekTakeHome
+//
+//  Created by Mohamad Rahmani on 15.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+

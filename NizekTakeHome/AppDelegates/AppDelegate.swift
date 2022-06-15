6b2758d72238d1e990205bd772361b7494a19d70
TheAlienMann <rahmanipour.mohammad@gmail.com>
2022-06-15 17:57:04 +0430

    initial commit

---
+++ NizekTakeHome/AppDelegates/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  NizekTakeHome
+//
+//  Created by Mohamad Rahmani on 15.06.22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  lazy var window: UIWindow? = UIWindow(frame: UIScreen.main.bounds)
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+
+    window?.rootViewController = MainViewController()
+    guard let vc = window?.rootViewController as? MainViewController else {
+      return true
+    }
+    window?.rootViewController = UINavigationController(rootViewController: vc)
+    window?.makeKeyAndVisible()
+    return true
+  }
+}
--- NizekTakeHome/Views/Controllers/MainViewController.swift
+++ NizekTakeHome/Views/Controllers/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  NizekTakeHome
 //
 //  Created by Mohamad Rahmani on 15.06.22.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    view.backgroundColor = .gray
   }
-
-
 }
-
